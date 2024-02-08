3a6054a323b7c9d0b3e81191703d0811559afa81
NatalyMit <mitenkova91@icloud.com>
2024-02-08 11:47:36 +0100

    add js

--- src/main.js
+++ src/main.js
@@ -1,121 +1,59 @@
-import createGallery from './createMarkup/articles.js';
-import { getGallery } from './js/galleryApi.js';
+import axios from 'axios';
 
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+async function fetchPokemon(pokemonId) {
+  const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+  return response.json();
+}
 
-const refs = {
-  formEl: document.querySelector('.form-search'),
-  galleryEL: document.querySelector('.gallery-images'),
-  loadMoreEl: document.querySelector('[data-action="load-more"]'),
-  preloader: document.getElementById('preloader'),
-  loaderGallery: document.querySelector('.loader-gallery'),
-};
+const cardContainer = document.querySelector('.card-container');
+const searchForm = document.querySelector('.search-form');
+const loaderEl = document.querySelector('.loader-gallery');
 
 const hiddenClass = 'is-hidden';
+searchForm.addEventListener('submit', onSearch);
 
-let query = '';
-let page = 1;
-let maxPage = 0;
+async function onSearch(e) {
+  e.preventDefault();
 
-refs.formEl.addEventListener('submit', handleSearch);
+  const form = e.currentTarget;
 
-async function handleSearch(event) {
-  event.preventDefault();
-
-  refs.galleryEL.innerHTML = '';
-  refs.loadMoreEl.classList.add(hiddenClass);
-  refs.loaderGallery.classList.remove(hiddenClass);
-
-  page = 1;
-
-  const form = event.currentTarget;
-  query = form.elements.query.value.trim();
-
-  if (!query) {
-    refs.loaderGallery.classList.add(hiddenClass);
-    refs.loadMoreEl.classList.add(hiddenClass);
-    iziToast.show({
-      title: '❌',
-      messageColor: 'white',
-      message: 'Sorry, You have not entered any word.Please try again!',
-      position: 'topRight',
-      color: 'yellow',
-    });
-    return;
-  }
-
+  const searchQuery = form.elements.query.value;
   try {
-    const { hits, totalHits } = await getGallery(query);
-    if (hits.length === 0) {
-      refs.loadMoreEl.classList.add(hiddenClass);
-      iziToast.show({
-        title: '❌',
-        messageColor: 'white',
-        message:
-          'Sorry, we dont have any pictures for your request.Please try again!',
-        messageSize: '25',
-        position: 'topRight',
-        color: 'yellow',
-      });
-      return;
-    }
-
-    maxPage = Math.ceil(totalHits / 40);
-
-    createGallery(hits);
-
-    if (hits.length > 0 && hits.length !== totalHits) {
-      refs.loadMoreEl.classList.remove(hiddenClass);
-      refs.loadMoreEl.addEventListener('click', handleLoadMore);
-    } else {
-      refs.loadMoreEl.classList.add(hiddenClass);
-    }
-  } catch (error) {
-    console.log(err);
+    const data = await fetchPokemon(searchQuery);
+    loaderEl.classList.remove(hiddenClass);
+    renderPokemonCard(data);
+  } catch {
+    onFetchError();
   } finally {
-    refs.loaderGallery.classList.add(hiddenClass);
+    loaderEl.classList.add(hiddenClass);
     form.reset();
   }
+
+  // .then(renderPokemonCard)
+  // .catch(onFetchError)
+  // .finally(form.reset);
 }
-async function handleLoadMore() {
-  page += 1;
 
-  refs.preloader.classList.remove(hiddenClass);
-  refs.loadMoreEl.classList.add(hiddenClass);
-  refs.loadMoreEl.disabled = true;
+function renderPokemonCard({ name, sprites, weight, height, abilities }) {
+  const abilityListItems = abilities
+    .map(ability => `<li class="list-group-item">${ability.name}</li>`)
+    .join('');
 
-  const getHeightImgCard = document
-    .querySelector('.list-item')
-    .getBoundingClientRect();
+  const markup = `<div class="card">
+<div class="card-img-top">
+<img src="${sprites.front_default}" alt="${name}></div>
+<div class="card-body">
+<h2 class="card-title">Ім'я: ${name}</h2>
+<p class="card-text">Вага: ${weight}</p>
+<p class="card-text">Зростання: ${height}</p>
 
-  try {
-    const { hits } = await getGallery(query, page);
+<p class="card-text"><b>Уміння</b></p>
+<ul class="list-group">${abilityListItems}</ul>
+</div>
+</div>`;
+  cardContainer.innerHTML = markup;
+}
 
-    createGallery(hits);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    window.scrollBy({
-      top: getHeightImgCard.height * 2,
-      left: 0,
-      behavior: 'smooth',
-    });
-
-    refs.preloader.classList.add(hiddenClass);
-    refs.loadMoreEl.disabled = false;
-    refs.loadMoreEl.classList.remove(hiddenClass);
-
-    if (page === maxPage) {
-      refs.loadMoreEl.classList.add(hiddenClass);
-      refs.loadMoreEl.removeEventListener('click', handleLoadMore);
-      iziToast.show({
-        title: 'Finish',
-        messageColor: 'white',
-        message: "We're sorry, but you've reached the end of search results.",
-        position: 'bottomCenter',
-        color: 'blue',
-      });
-    }
-  }
+function onFetchError(error) {
+  alert('Упс, щось пішло не так і ми не знайшли вашого покемона!');
 }
